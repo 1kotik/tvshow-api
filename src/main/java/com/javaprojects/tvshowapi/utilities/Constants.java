2849791af7b420f823ae781bf3fe252fb231db27
1kotik <lyoshakotik@gmail.com>
2024-03-25 19:35:26 +0300

    constructor fix

--- src/main/java/com/javaprojects/tvshowapi/utilities/Constants.java
+++ src/main/java/com/javaprojects/tvshowapi/utilities/Constants.java
@@ -1,6 +1,10 @@
 package com.javaprojects.tvshowapi.utilities;
 
+
 public class Constants {
+    private Constants(){
+        
+    }
     public static final String INVALID_INFO_MSG = "Invalid info provided";
     public static final String NOT_FOUND_MSG = "Required resources are not found";
     public static final String SERVER_ERROR_MSG = "Some error on server occurred";
